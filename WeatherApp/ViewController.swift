e2d5d9d17b4cefb79ec6b90c79c050955ff0f580
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2024-01-04 18:29:35 +0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Akbar Khusanbaev on 04/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
